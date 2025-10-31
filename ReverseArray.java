0145ff3d4769d9fbfa2f56ecb0984509e67cd588
LaxmiprasannaBandam <24wh1a05w6@bvrithyderabad.edu.in>
2025-10-31 18:19:20 +0530

    Add ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,58 @@
+// Problem: Reverse an array using user input
+// Date: 2025-10-31
+// Author: <Your Name>
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    // Method to reverse the array
+    public static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    // Method to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking array size as input
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Taking array elements as input
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Original Array: ");
+        printArray(arr);
+
+        // Reverse the array
+        reverse(arr);
+
+        System.out.print("Reversed Array: ");
+        printArray(arr);
+
+        sc.close();
+    }
+}
