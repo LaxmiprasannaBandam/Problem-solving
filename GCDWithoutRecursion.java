03def72b1c1bec24c85e58405086a7464911b905
LaxmiprasannaBandam <24wh1a05w6@bvrithyderabad.edu.in>
2025-11-04 21:58:11 +0530

    Add  GCDWithoutRecursion.java

---
+++ GCDWithoutRecursion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GCDWithoutRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Euclidean algorithm using a loop
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        System.out.println("GCD or HCF is: " + num1);
+        sc.close();
+    }
+}
