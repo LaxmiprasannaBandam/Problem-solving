c073d824b54df6b0471427d94475b5ffa55e301b
LaxmiprasannaBandam <24wh1a05w6@bvrithyderabad.edu.in>
2025-11-16 18:51:42 +0530

    Add README.md add two numbers.java

---
+++ README.md add two numbers.java
@@ -0,0 +1,60 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) { 
+        this.val = val; 
+    }
+
+    ListNode(int val, ListNode next) { 
+        this.val = val; 
+        this.next = next; 
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Print Output
+        System.out.print("Output: ");
+        while (result != null) {
+            System.out.print(result.val);
+            if (result.next != null) System.out.print(" -> ");
+            result = result.next;
+        }
+    }
+}
