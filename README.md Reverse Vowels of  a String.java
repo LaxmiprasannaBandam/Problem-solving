3cdc93568c9cc1c216a1d5258aaa4996345b3d53
LaxmiprasannaBandam <24wh1a05w6@bvrithyderabad.edu.in>
2025-12-17 18:44:07 +0530

    Add README.md Reverse Vowels of  a String.java

---
+++ README.md Reverse Vowels of  a String.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Solution {
+    public String reverseVowels(String s) {
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+        
+        while (start < end) {
+            // Move start pointer until it points to a vowel
+            while (start < end && vowels.indexOf(word[start]) == -1) {
+                start++;
+            }
+            
+            // Move end pointer until it points to a vowel
+            while (start < end && vowels.indexOf(word[end]) == -1) {
+                end--;
+            }
+            
+            // Swap the vowels
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+            
+            // Move the pointers towards each other
+            start++;
+            end--;
+        }
+        
+        return new String(word);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        Solution sol = new Solution();
+        String result = sol.reverseVowels(input);
+        
+        System.out.println("String after reversing vowels: " + result);
+        
+        sc.close();
+    }
+}
